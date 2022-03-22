0415970a21ecfb177684747e4982299de47876a1
Ebrahim Ahmed <ebrahimahmed97090@gmail.com>
2022-03-22 20:53:29 +0200

    update to ver 4.2

--- config/database.js
+++ config/database.js
@@ -3,10 +3,10 @@ module.exports = ({ env }) => ({
     client: 'mysql',
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 3306),
+      port: env.int('DATABASE_PORT', 6603),
       database: env('DATABASE_NAME', 'learn'),
       user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'password'),
+      password: env('DATABASE_PASSWORD', 'example'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
